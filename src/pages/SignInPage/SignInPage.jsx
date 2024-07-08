0807c534e5659e2b3c5d0a421d5326d12bfc61d6
KhoaPhan1504 <Khoahttt01@gmail.com>
2024-07-08 15:08:42 +0700

    Build SignInPage, SignUpPage

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import { WrapperContainerLeft, WrapperContainerRight, WrapperTextLight } from './style';
+import InputForm from '../../components/InputFormComponent/InputForm';
+import ButtonComponent from '../../components/ButtonComponent/ButtonComponent';
+import { Image } from 'antd';
+import imageLogo from '../../assests/login/login.png';
 
 const SignInPage = () => {
   return (
-    <div>SignInPage</div>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'rgba(0, 0, 0, 0.53)', height: '100vh'}}>
+      <div style={{display: 'flex', width: '800px', height: '527px', borderRadius: '6px', backgroundColor: '#fff'}}>
+        <WrapperContainerLeft>
+          <h1>Xin chào</h1>
+          <p>Đăng nhập hoặc Tạo tài khoản</p>
+          <InputForm style={{ marginBottom: '10px'}} placeholder='abc@gmail.com'/>
+          <InputForm placeholder='password'/>
+          <ButtonComponent
+            bordered={false}
+            size={40} 
+            styleButton={{
+              background: 'rgb(255, 57, 69)',
+              height: '48px',
+              width: '100%',
+              border: 'none',
+              borderRadius: '4px',
+              margin: '26px 0 10px'
+            }}
+            textButton={"Đăng nhập"}
+            styleTextButton={{ color: '#fff', fontSize: '15px', fontWeight: '700' }}
+          ></ButtonComponent>
+          <p><WrapperTextLight>Quên mật khẩu?</WrapperTextLight></p>
+          <p>Chưa có tài khoản? <WrapperTextLight>Tạo tài khoản</WrapperTextLight></p>
+        </WrapperContainerLeft>
+        <WrapperContainerRight>
+          <Image src={imageLogo} preview={false} alt='Logo Login' height='203px' width='203px' />
+          <h4>Mua sắm thả ga tại Sakila</h4>
+        </WrapperContainerRight>
+      </div>
+    </div>
   )
 }
 
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import ButtonComponent from '../../components/ButtonComponent/ButtonComponent';
+import InputForm from '../../components/InputFormComponent/InputForm';
+import { WrapperContainerLeft, WrapperContainerRight, WrapperTextLight } from './style';
+import imageLogo from '../../assests/login/login.png';
+import { Image } from 'antd';
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'rgba(0, 0, 0, 0.53)', height: '100vh'}}>
+      <div style={{display: 'flex', width: '800px', height: '527px', borderRadius: '6px', backgroundColor: '#fff'}}>
+        <WrapperContainerLeft>
+          <h1>Xin chào</h1>
+          <p>Đăng nhập hoặc Tạo tài khoản</p>
+          <InputForm style={{ marginBottom: '10px'}} placeholder='abc@gmail.com'/>
+          <InputForm style={{ marginBottom: '10px'}} placeholder='password'/>
+          <InputForm placeholder='confirm password'/>
+          <ButtonComponent
+            bordered={false}
+            size={40} 
+            styleButton={{
+              background: 'rgb(255, 57, 69)',
+              height: '48px',
+              width: '100%',
+              border: 'none',
+              borderRadius: '4px',
+              margin: '26px 0 10px'
+            }}
+            textButton={"Đăng ký"}
+            styleTextButton={{ color: '#fff', fontSize: '15px', fontWeight: '700' }}
+          ></ButtonComponent>
+          <p>Bạn đã có tài khoản? <WrapperTextLight>Đăng nhập</WrapperTextLight></p>
+        </WrapperContainerLeft>
+        <WrapperContainerRight>
+          <Image src={imageLogo} preview={false} alt='Logo Login' height='203px' width='203px' />
+          <h4>Mua sắm thả ga tại Sakila</h4>
+        </WrapperContainerRight>
+      </div>
+    </div>
   )
 }
 
